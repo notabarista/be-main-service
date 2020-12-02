2df153d47bd5db7d4c631893131c2e3aa0a777d7
tudor <codrea_tudor@yahoo.com>
2020-12-02 22:55:10 +0200

    adding proper doc, admin service client and boot version 2.2.4
    downgrade to spring boot 2.2.6
    adds admin service client
    adding proper swagger api doc integration

--- src/main/java/com/notabarrista/MainServiceApplication.java
+++ src/main/java/com/notabarrista/MainServiceApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author codrea.tudor
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @SpringBootApplication
 @ComponentScan
-@EnableWebMvc
+@EnableSwagger2
 @EnableAutoConfiguration
 public class MainServiceApplication {
 
